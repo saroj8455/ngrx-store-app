7db5387b993b740fbf1e66ac92e2097ca99f3942
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2024-12-31 20:08:25 +0530

    implement counter state as centralize with ngrx

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,34 @@
+import { AsyncPipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { decrement, increment, reset } from './counter.action';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AsyncPipe],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'ngrx-store-app';
+  count$!: Observable<number>;
+  constructor(private store: Store<{ count: number }>) {
+    // TODO: Connect `this.count$` stream to the current store `count` state
+    this.count$ = store.select('count');
+  }
+
+  inc() {
+    console.log('dispatch increment action');
+    this.store.dispatch(increment());
+  }
+  dec() {
+    console.log('dispatch decrement action');
+    this.store.dispatch(decrement());
+  }
+  res() {
+    this.store.dispatch(reset());
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,9 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideStore, StoreModule } from '@ngrx/store';
+import { counterReducer } from './counter.reducer';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideStore({ count: counterReducer })],
 };
