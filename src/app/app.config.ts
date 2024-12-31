2eed7b9b9c4832a577a143e0d9299d6925b0b22a
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2024-12-31 20:18:50 +0530

    ngrx store  integration  in UI

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,18 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideStore, StoreModule } from '@ngrx/store';
 import { counterReducer } from './counter.reducer';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideStore({ count: counterReducer })],
+  providers: [
+    provideRouter(routes),
+    provideStore({ count: counterReducer }),
+    provideStoreDevtools({
+      maxAge: 25, // Retain last 25 states
+      logOnly: !isDevMode(), // Ensures DevTools are enabled only in development mode.
+    }),
+  ],
 };
